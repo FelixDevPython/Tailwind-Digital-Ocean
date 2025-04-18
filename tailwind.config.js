0b7d5475527c1d9ac3d6faecf246e75c2c1e3aab
Felix Miranda <felix.miranda.sandoval@gmail.com>
2025-04-17 22:35:41 -0400

    Inicio del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html','./js/*.js',],
+  theme: {
+    extend: {
+      spacing: {
+        'quarter': '25%',
+      },
+      fontFamily: {
+        'sans':"'Work Sans', sans-serif",
+        'cascadia':'"CascadiaCode"',
+      },
+      colors: {
+        'do-blue-dark':'#080c2d',
+        'do-blue-medium':'rgb(20, 86, 255)',
+        'do-blue-light':'rgb(0, 105, 255)',
+      },
+    },
+  },
+  plugins: [],
+}
+
