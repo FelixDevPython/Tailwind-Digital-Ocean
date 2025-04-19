773f6fb39755b1033f3ea106226d88200bc6564b
Felix Miranda <felix.miranda.sandoval@gmail.com>
2025-04-18 20:21:53 -0400

    Primera etapa de la maquetacion del sitio web, creación de Barra de navegacion y aplicaion de estados hover. focus y active en los elementos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ['./*.html','./js/*.js',],
   theme: {
+    container:{
+      padding: '1.4rem',
+    },
     extend: {
       spacing: {
         'quarter': '25%',
@@ -11,6 +14,7 @@ module.exports = {
       },
       colors: {
         'do-blue-dark':'#080c2d',
+        'do-blue-mediumdark':'#1d274c',
         'do-blue-medium':'rgb(20, 86, 255)',
         'do-blue-light':'rgb(0, 105, 255)',
       },
