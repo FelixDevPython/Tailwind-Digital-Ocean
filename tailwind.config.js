12fcd9175a613adf25e51d347c692f2aa4fa46be
Felix Miranda <felix.miranda.sandoval@gmail.com>
2025-04-24 23:05:52 -0400

    Implementación de animaciones y modo oscuro en el proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class',
   content: ['./*.html','./js/*.js',],
   theme: {
     container:{
@@ -8,6 +9,11 @@ module.exports = {
       }
     },
     extend: {
+      transitionTimingFunction: {
+        'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+        'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+        'elastic': 'cubic-bezier(0.29, 1.01, 1, -0.68)',
+      },
       spacing: {
         'quarter': '25%',
       },
